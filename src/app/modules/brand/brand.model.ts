6e1156c6596e5f188ea2138f1fb0a09a7420de28
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 11:12:11 +0600

    feat: add brandSchema model

---
+++ src/app/modules/brand/brand.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+import { TBrand } from "./brand.interface";
+
+const brandSchema = new Schema<TBrand>({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+export const Brand = model("Brand", brandSchema)
