a5cedd196f94d5c0cf35bbfe2fcf66d91e171fcc
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 10:24:07 +0600

    feat: add newsSchema model

---
+++ src/app/modules/news/news.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+import { TNews } from "./news.interface";
+
+const newsSchema = new Schema<TNews>({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+)
+
+export const News = model("News", newsSchema)
