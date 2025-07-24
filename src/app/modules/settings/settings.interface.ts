67caf472f88858afa3c3eb06656e0016ec5bd717
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 10:22:16 +0600

    feat: add TNews interface

--- src/app/modules/settings/settings.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-// Define the interface for your settings
-export interface ISettings extends Document {
-     privacyPolicy: string;
-     aboutUs: string;
-     support: string;
-     termsOfService: string;
-}
