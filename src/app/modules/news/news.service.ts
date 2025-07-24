2a32d048c45417aa9abf0e01f00623a4aa558996
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 10:41:45 +0600

    feat: add news delete service

--- src/app/modules/news/news.service.ts
+++ src/app/modules/news/news.service.ts
@@ -1,7 +1,9 @@
+import mongoose from "mongoose";
 import AppError from "../../../errors/AppError";
 import unlinkFile from "../../../shared/unlinkFile";
 import { TNews } from "./news.interface";
 import { News } from "./news.model";
+import { StatusCodes } from "http-status-codes";
 
 const createNewsToDB = async (payload: TNews) => {
     const result = await News.create(payload);
@@ -40,9 +42,26 @@ const updateNewsById = async (id: string, updatedPayload: Partial<TNews>) => {
     return news;
 }
 
+const deleteNewsById = async (id: string) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new AppError(StatusCodes.NOT_ACCEPTABLE, 'Invalid ');
+    }
+
+    const isNewsExist: any = await News.findById({ _id: id });
+
+    //delete from folder
+    if (isNewsExist) {
+        unlinkFile(isNewsExist?.image);
+    }
+
+    const result = await News.findByIdAndDelete(id);
+    return result;
+}
+
 export const NewsServices = {
     createNewsToDB,
     getNewsFromDB,
     getNewsById,
     updateNewsById,
+    deleteNewsById,
 }
