81acddfc45f84a12af919b4df26e71c1716539a1
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 14:40:55 +0600

    feat: add create review service

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,44 +1,28 @@
-import mongoose from 'mongoose';
 import { IReview } from './review.interface';
 import { Review } from './review.model';
 import { StatusCodes } from 'http-status-codes';
-import { User } from '../user/user.model';
 import AppError from '../../../errors/AppError';
 
-const createReviewToDB = async (payload: IReview): Promise<IReview> => {
-     // Fetch baber and check if it exists in one query
-     const user: any = await User.findById(payload.barber);
-     if (!user) {
-          throw new AppError(StatusCodes.NOT_FOUND, 'No User Found');
+const createReviewToDB = async (payload: IReview) => {
+     const { user, product, rating } = payload;
+
+     // rating must be between 1 and 5
+     if (rating < 1 || rating > 5) {
+          throw new AppError(StatusCodes.BAD_REQUEST, 'Rating must be between 1 and 5');
      }
 
-     if (payload.rating) {
-          // checking the rating is valid or not;
-          const rating = Number(payload.rating);
-          if (rating < 1 || rating > 5) {
-               throw new AppError(StatusCodes.BAD_REQUEST, 'Invalid rating value');
-          }
 
-          // Update service's rating and total ratings count
-          const ratingCount = user.ratingCount + 1;
+     const existingReview = await Review.findOne({ user, product });
 
-          let newRating;
-          if (user.rating === null || user.rating === 0) {
-               // If no previous ratings, the new rating is the first one
-               newRating = rating;
-          } else {
-               // Calculate the new rating based on previous ratings
-               newRating = (user.rating * user.ratingCount + rating) / ratingCount;
-          }
-
-          await User.findByIdAndUpdate({ _id: payload.barber }, { rating: parseFloat(newRating.toFixed(2)), ratingCount: ratingCount }, { new: true });
+     if (existingReview) {
+          throw new AppError(StatusCodes.CONFLICT, 'You have already reviewed this product.');
      }
+
 
      const result = await Review.create(payload);
-     if (!result) {
-          throw new AppError(StatusCodes.BAD_REQUEST, 'Failed To create Review');
-     }
-     return payload;
+     return result;
 };
 
-export const ReviewService = { createReviewToDB };
+export const ReviewServices = {
+     createReviewToDB,
+}
