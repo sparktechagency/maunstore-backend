b492ff111f0c0e4fbb002a355bf03d8e228bef85
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 12:15:46 +0600

    feat: add product CRUD operations  controllers

---
+++ src/app/modules/product/product.controller.ts
@@ -0,0 +1,66 @@
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { ProductServices } from "./product.service";
+
+const createProduct = catchAsync(async (req, res) => {
+    const productData = req.body;
+    const result = await ProductServices.createProductToDB(productData);
+    sendResponse(res, {
+        success: true,
+        statusCode: 200,
+        message: "Product is created successfully",
+        data: result,
+    })
+})
+
+const getProducts = catchAsync(async (req, res) => {
+    const result = await ProductServices.getProductsFromDB();
+    sendResponse(res, {
+        success: true,
+        statusCode: 200,
+        message: "Products data are retrieved successfully",
+        data: result,
+    })
+})
+
+const getProductById = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await ProductServices.getProductByIdFromDB(id);
+    sendResponse(res, {
+        success: true,
+        statusCode: 200,
+        message: "Product data is retrieved successfully",
+        data: result,
+    })
+})
+
+const updateProductById = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const updatedPayload = req.body;
+    const result = await ProductServices.updateProductByIdToDB(id, updatedPayload);
+    sendResponse(res, {
+        success: true,
+        statusCode: 200,
+        message: "Product data is updated successfully",
+        data: result,
+    })
+})
+
+const deleteProductById = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await ProductServices.deleteNewsByIdFromDB(id);
+    sendResponse(res, {
+        success: true,
+        statusCode: 200,
+        message: "Product is deleted successfully",
+        data: result,
+    })
+})
+
+export const ProductControllers = {
+    createProduct,
+    getProducts,
+    getProductById,
+    updateProductById,
+    deleteProductById,
+}
