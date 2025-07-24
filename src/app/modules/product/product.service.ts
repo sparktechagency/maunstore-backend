22d212aec9ce2dc8547b16d320a68e2f17cd7593
Moshfiqur Rahman <studentemam@gmail.com>
2025-07-24 12:02:14 +0600

    feat: product getById, update, and delete logic

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,7 +1,9 @@
+import mongoose from "mongoose";
 import AppError from "../../../errors/AppError";
 import unlinkFile from "../../../shared/unlinkFile";
 import { TProduct } from "./product.interface";
 import { Product } from "./product.model";
+import { StatusCodes } from "http-status-codes";
 
 const createProductToDB = async (payload: TProduct) => {
     const result = await Product.create(payload);
@@ -12,17 +14,58 @@ const createProductToDB = async (payload: TProduct) => {
 }
 
 const getProductsFromDB = async () => {
-    const result = await Product.find();
+    const result = await Product.find().populate({ path: "brand" });
     if (!result || result.length === 0) {
         throw new AppError(404, "No products are found in the database")
     };
     return result;
 }
 
+const getProductByIdFromDB = async (id: string) => {
+    const result = await Product.findById(id).populate({ path: "brand" });
+    if (!result) {
+        throw new AppError(404, "No product is found in the database")
+    };
+    return result;
+}
 
+const updateProductByIdToDB = async (id: string, updatedPayload: Partial<TProduct>) => {
+    const isProductExist = await Product.findById(id);
+
+    if (!isProductExist) {
+        throw new AppError(404, "No product is found for this product ID")
+    }
+
+    if (updatedPayload.images && isProductExist?.images) {
+        unlinkFile(isProductExist?.images);
+    }
+
+    const news = await Product.findByIdAndUpdate(id, updatedPayload, { new: true }).populate({ path: "brand" });
+
+    return news;
+}
+
+const deleteNewsByIdFromDB = async (id: string) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new AppError(StatusCodes.NOT_ACCEPTABLE, 'Invalid ');
+    }
+
+    const isProductExist: any = await Product.findById({ _id: id });
+
+    //delete from folder
+    if (isProductExist) {
+        unlinkFile(isProductExist?.images);
+    }
+
+    const result = await Product.findByIdAndDelete(id);
+    return result;
+};
 
 export const ProductServices = {
     createProductToDB,
     getProductsFromDB,
+    getProductByIdFromDB,
+    updateProductByIdToDB,
+    deleteNewsByIdFromDB,
 }
 
