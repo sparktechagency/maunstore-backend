34532476a55d9c5046d9673f67bf79a2aa587718
Moshfiqur Rahman <studentemam@gmail.com>
2025-08-13 14:41:07 +0600

    feat: add category schema design

---
+++ src/app/modules/category/category.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+import { TCategory } from "./category.interface";
+
+const categorySchema = new Schema<TCategory>({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+        default: ""
+    },
+    brandId: {
+        type: Schema.Types.ObjectId,
+        ref: "Brand"
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+export const Category = model("Category", categorySchema)
